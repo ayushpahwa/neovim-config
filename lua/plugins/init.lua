7b0275f693764427caca287f89d6b953b783d52c
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-09-17 18:26:49 +0530

    update: configs for telescope plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -75,4 +75,23 @@ return {
       "nvim-lua/plenary.nvim",
     },
   },
+  -- Fuzzy Finder (files, lsp, etc)
+  {
+    "nvim-telescope/telescope.nvim",
+    event = "VimEnter",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable "make" == 1
+        end,
+      },
+      { "nvim-telescope/telescope-ui-select.nvim" },
+    },
+    config = function()
+      require "configs.telescope"
+    end,
+  },
 }
