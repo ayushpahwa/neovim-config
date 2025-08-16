30f13a855b35576aed03783f107dd8eafaa55941
Ayush Pahwa <ayushpahwa96@gmail.com>
2025-08-17 01:50:19 +0530

    update: added tmux nav support

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -61,7 +61,56 @@ keymap.set(
 )
 
 -- Diagnostics
-keymap.set("n", "<C-j>", function()
-  vim.diagnostic.goto_next()
+keymap.set("n", "<C-m>", function()
+  vim.diagnostic.jump { count = 1 }
 end, opts)
 keymap.set("n", "gy", ":cclose<Return>", add_description "Close the suggestions window")
+
+-- Debugging with nvim-dap
+local dap = require "dap"
+local dapui = require "dapui"
+
+-- Toggle breakpoint
+keymap.set("n", "<Leader>dp", ":lua require'dap'.toggle_breakpoint()<CR>", add_description "Toggle breakpoint")
+
+-- Start debugging (launch mode)
+keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>", add_description "Start / Continue Debugging")
+
+-- Stop debugging
+keymap.set("n", "<F6>", ":lua require'dap'.terminate()<CR>", add_description "Stop Debugging")
+
+-- Restart debugging session
+keymap.set("n", "<F8>", ":lua require'dap'.restart()<CR>", add_description "Restart Debugging")
+
+-- Step over (next line without entering function)
+keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>", add_description "Step Over")
+
+-- Step into (enter function)
+keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>", add_description "Step Into")
+
+-- Step out (exit function)
+keymap.set("n", "<S-F11>", ":lua require'dap'.step_out()<CR>", add_description "Step Out")
+
+-- Open DAP UI
+keymap.set("n", "<Leader>du", ":lua require'dapui'.toggle()<CR>", add_description "Toggle DAP UI")
+
+-- Show variables in floating window
+keymap.set("n", "<Leader>dv", ":lua require'dapui'.eval()<CR>", add_description "Evaluate expression")
+
+-- Show stack trace
+keymap.set("n", "<Leader>dto", ":lua require'dap'.repl.open()<CR>", add_description "Show stack trace: DAP REPL")
+
+-- Hide stack trace
+keymap.set("n", "<Leader>dtc", ":lua require'dap'.repl.close()<CR>", add_description "Hide stack trace: DAP REPL")
+
+-- Show breakpoints list
+keymap.set("n", "<Leader>db", ":lua require'dap'.list_breakpoints()<CR>", add_description "List all breakpoints")
+
+-- Clear all breakpoints
+keymap.set("n", "<Leader>dc", ":lua require'dap'.clear_breakpoints()<CR>", add_description "Clear all breakpoints")
+
+keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", add_description "Move left")
+keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", add_description "Move right")
+keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", add_description "Move down")
+keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", add_description "Move up")
+keymap.set("n", "<C-\\>", ":TmuxNavigatePrevious<CR>", add_description "Move to last window")
