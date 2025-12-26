6e3a7eb7aeb1c7f04f9cc37c69fe0ca41c7e530c
Ayush Pahwa <ayush.pahwa@automattic.com>
2025-12-26 13:16:04 +0700

    update: add servers for lsp and autocomplete

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,7 +2,7 @@ local nvlsp = require "nvchad.configs.lspconfig"
 local lspconfig = require "lspconfig"
 
 nvlsp.defaults()
-local servers = { "html", "cssls", "ts_ls", "lua_ls", "eslint", "intelephense" }
+local servers = { "html", "cssls", "ts_ls", "lua_ls", "eslint", "intelephense", "ruff", "pyright" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
